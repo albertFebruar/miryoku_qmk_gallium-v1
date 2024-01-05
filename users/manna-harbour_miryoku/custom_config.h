959f035d7bfccf6adba0e44d8d593c4a2013dd4f
albertFebruar <154378559+albertFebruar@users.noreply.github.com>
2024-01-05 15:50:56 +0100

    Update custom_config.h with actual galliumv1 key layout

--- users/manna-harbour_miryoku/custom_config.h
+++ users/manna-harbour_miryoku/custom_config.h
@@ -6,7 +6,7 @@
 #pragma once
 // temp, keycodes to be updated
 #define MIRYOKU_ALTERNATIVES_BASE_GALLIUMV1 \
-KC_Q,              KC_W,              KC_F,              KC_P,              KC_B,              KC_J,              KC_L,              KC_U,              KC_Y,              KC_QUOT,           \
-LGUI_T(KC_A),      LALT_T(KC_R),      LCTL_T(KC_S),      LSFT_T(KC_T),      KC_G,              KC_M,              LSFT_T(KC_N),      LCTL_T(KC_E),      LALT_T(KC_I),      LGUI_T(KC_O),      \
-LT(U_BUTTON,KC_Z), ALGR_T(KC_X),      KC_C,              KC_D,              KC_V,              KC_K,              KC_H,              KC_COMM,           ALGR_T(KC_DOT),    LT(U_BUTTON,KC_SLSH),\
+KC_B,              KC_L,              KC_D,              KC_C,              KC_V,              KC_Z,              KC_Y,              KC_O,              KC_U,              KC_COMM,           \
+LGUI_T(KC_N),      LALT_T(KC_R),      LCTL_T(KC_T),      LSFT_T(KC_S),      KC_G,              KC_P,              LSFT_T(KC_H),      LCTL_T(KC_A),      LALT_T(KC_E),      LGUI_T(KC_I),      \
+LT(U_BUTTON,KC_Q), ALGR_T(KC_X),      KC_M,              KC_W,              KC_J,              KC_K,              KC_F,              KC_QUOT,           ALGR_T(KC_SCLN),    LT(U_BUTTON,KC_DOT),\
 U_NP,              U_NP,              LT(U_MEDIA,KC_ESC),LT(U_NAV,KC_SPC),  LT(U_MOUSE,KC_TAB),LT(U_SYM,KC_ENT),  LT(U_NUM,KC_BSPC), LT(U_FUN,KC_DEL),  U_NP,              U_NP
